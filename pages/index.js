678b9bfce212d80856371e7dbdf6fb38a1b9bc9d
Lekan Dar <Lekandar11@gmail.com>
2022-04-29 16:45:50 -1200

    version-four

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from "next/head";
-import Link from "next/link";
+import { SkewLoader } from "react-spinners";
 import ImageGallery from "react-image-gallery";
 import { SayButton } from "react-say";
 import { Menu } from "@headlessui/react";
 import { GiSpeaker } from "react-icons/gi";
 import ModalMessage from "./components/Modal";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   const images = [
     {
       original: "/p1.png",
@@ -29,38 +37,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section>
-        <div className="flex items-center justify-center py-8">
-          <img src="/logo.png" alt="logo" />
+      {loading ? (
+        <div className="flex items-center justify-center min-h-screen">
+          <SkewLoader color="#000" loading={loading} />
         </div>
-        <div className="w-full ">
-          <ImageGallery
-            items={images}
-            showNav={false}
-            showPlayButton={false}
-            autoPlay={true}
-            slideDuration={500}
-          />
-        </div>
-
-        <section className="space-y-4 px-4 lg:px-12 my-8 text-center">
-          <div>
-            <h1 className="text-lg font-semibold lg:text-3xl text-gray-800 py-4 uppercase">
-              The Launin Kasa Tote is here
-            </h1>
-
-            <button className="btn ">
-              <SayButton speak="LAUNIN KASA TOTE">
-                <span className="capitalize text-xs lg:text-base">
-                  <GiSpeaker />
-                </span>
-              </SayButton>
-            </button>
-
-            <ModalMessage />
+      ) : (
+        <section>
+          <div className="flex items-center justify-center py-8">
+            <img src="/logo.png" alt="logo" />
           </div>
+          <div className="w-full ">
+            <ImageGallery
+              items={images}
+              showNav={false}
+              showPlayButton={false}
+              autoPlay={true}
+              slideDuration={500}
+            />
+          </div>
+
+          <section className="space-y-4 px-4 lg:px-12 my-8 text-center">
+            <div>
+              <h1 className="text-lg font-semibold lg:text-3xl text-gray-800 py-4 uppercase">
+                The Launin Kasa Tote is here
+              </h1>
+
+              <button className="btn ">
+                <SayButton speak="LAUNIN KASA TOTE">
+                  <span className="capitalize text-xs lg:text-base">
+                    <GiSpeaker />
+                  </span>
+                </SayButton>
+              </button>
+
+              <ModalMessage />
+            </div>
+          </section>
         </section>
-      </section>
+      )}
     </div>
   );
 }
